f115664bff3087bcf00cd4087fff2ce13cc2eede
solobaba <solobaba_odu@yahoo.com>
2018-06-28 15:17:59 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mighty/journalapp/AddTaskActivity.java
@@ -0,0 +1,73 @@
+package com.example.mighty.journalapp;
+
+import android.content.ContentValues;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import static com.example.mighty.journalapp.data.TaskContract.*;
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    // Declare a member variable to keep track of a task's selected mPriority
+    private int mPriority;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+
+        getSupportActionBar().setTitle("Add a New Task");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Initialize to highest mPriority by default (mPriority = 1)
+        ((RadioButton) findViewById(R.id.radButton1)).setChecked(true);
+        mPriority = 1;
+    }
+
+    //onClickAddTask is called when the "ADD" button is clicked
+    //It retrieves user input and inserts that new task data into the underlying database
+    public void onClickAddTask(View view){
+        // Not yet implemented
+        // Check if EditText is empty, if not retrieve input and store it in a ContentValues object
+        // If the EditText input is empty -> don't create an entry
+        String input = ((EditText) findViewById(R.id.editTextTaskDescription)).getText().toString();
+        if(input.length() == 0){
+            return;
+        }
+
+        // Insert new task data via a ContentResolver
+        // Create new empty ContentValues object
+        ContentValues contentValues = new ContentValues();
+        // Put the task description and selected mPriority into the ContentValues
+        contentValues.put(TaskEntry.COLUMN_DESCRIPTION, input);
+        contentValues.put(TaskEntry.COLUMN_PRIORITY, mPriority);
+        // Insert the content values via a ContentResolver
+        Uri uri = getContentResolver().insert(TaskEntry.CONTENT_URI, contentValues);
+
+        // Display the URI that's returned with a Toast
+        // [Hint] Don't forget to call finish() to return to MainActivity after this insert is complete
+        if (uri != null){
+            Toast.makeText(getApplication(), uri.toString(), Toast.LENGTH_LONG).show();
+        }
+
+        // Finish activity (this returns back to MainActivity)
+        finish();
+    }
+
+    //onPrioritySelected is called whenever a priority button is clicked.
+    //It changes the value of mPriority based on the selected button.
+    public void onPrioritySelected(View view){
+        if (((RadioButton) findViewById(R.id.radButton1)).isChecked()){
+            mPriority = 1;
+        } else if (((RadioButton) findViewById(R.id.radButton2)).isChecked()){
+            mPriority = 2;
+        } else if (((RadioButton) findViewById(R.id.radButton3)).isChecked()){
+            mPriority = 3;
+        }
+    }
+}
